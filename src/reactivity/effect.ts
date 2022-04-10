73350c098fa410f64d47d5753e3592aba4f0a718
unknown <nanami2962@gmail.com>
2022-04-10 13:28:18 +0800

    feat: implement effect stop and onStop

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,10 @@
+import { extend } from "../shared/index";
+
 class ReactiveEffect {
   private _fn;
+  deps = [];
+  active = true;
+  onStop?: () => void;
   constructor(fn, public scheduler?) {
     this._fn = fn;
   }
@@ -7,6 +12,21 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn();
   }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if (this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
 }
 
 // this line will run automatically when this file called
@@ -25,7 +45,10 @@ export function track(target, key) {
     depsMap.set(key, dep);
   }
 
+  if (!activeEffect) return;
+
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
@@ -43,9 +66,17 @@ export function trigger(target, key) {
 
 let activeEffect;
 export function effect(fn, options: any = {}) {
-  const scheduler = options.scheduler;
-  const _effect = new ReactiveEffect(fn, scheduler);
+  const _effect = new ReactiveEffect(fn, options.scheduler);
+  // _effect.onStop = options.onStop;
+  // exetend
+  extend(_effect, options);
   _effect.run();
-  const runner = _effect.run.bind(_effect);
+
+  const runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
   return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
